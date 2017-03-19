5578be8a66c5bb25ed38371529c680bfc2a80fd7
Adam Skonieczny <askonieczny@umass.edu>
2017-03-19 18:31:44 -0400

    quick mimic of ughworld for league

--- app/app.js
+++ app/app.js
@@ -1,14 +1,12 @@
-//import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Each major browser view user interface must be imported.
-import League from './components/league.js';
-
+import League from './components/league.js'
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
-if (document.getElementById('league') !== null) {
+
   ReactDOM.render(
     <League />,
     document.getElementById('league')
-  );
-}
+  )
