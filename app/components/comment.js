cbbac4ede19b5e827f2d20af5c5ea1b1769cd43c
Hisham Alhussain <halhussain@umass.edu>
2017-03-28 15:07:12 -0400

    fixed styling issues with highlights

--- app/components/comment.js
+++ app/components/comment.js
@@ -3,18 +3,18 @@ import React from 'react';
 export default class Comment extends React.Component{
   render(){
     return(
-      <li className="media">
+      <div className="media">
           <div className="media-left media-top">
             <span className="media-left media-top">
               <img src={this.props.avatar} alt="avatar" className="img-circle" height="35" width="35"></img>
             </span>
           </div>
           <div className="media-body">
-              <a href="#">{this.props.user}</a> 
+              <a href="#">{this.props.user}</a>
                 {this.props.message}
               <br /><a href="#">Like</a> · <a href="#">Reply</a> · {this.props.timestamp}
           </div>
-      </li>
+      </div>
     )
   }
 }
