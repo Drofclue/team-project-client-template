41af681cb471a32dede353246aeaf22656842570
Peter Davey <pdavey@umass.edu>
2017-04-09 17:49:21 -0400

    initial attempt at pulling data from database for userprofile page

--- app/components/userprofilepage.js
+++ app/components/userprofilepage.js
@@ -3,15 +3,29 @@ import LeftNavBar from './leftnavbar';
 import RightSideBar from './rightsidebar';
 import Footer from './footer.js';
 import UserProfile from './userprofile.js';
+import {getUserData} from '../server';
 
 export default class UserProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents : []
+    };
+  }
+
+  refresh(){
+    getUserData(this.props.user, (userData) => {
+      this.setState(userData);
+    });
+  }
+
   render(){
     return(
       <div className="container-fluid text-center">
         <div className="row content">
           <LeftNavBar />
           <div className="col-md-7 text-left">
-            <UserProfile />
+            <UserProfile userData={this.refresh()}/>
           </div>
           <RightSideBar />
         </div>
