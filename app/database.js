98ba51c3c25db5ed85139cb0939c9863d7acc25a
Thomas Aurigemma <taaurigemma@gmail.com>
2017-04-09 13:44:16 -0400

    Edited some pages trying to implement the reset db button

--- app/database.js
+++ app/database.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Modify with your startup's name!
-var startupName = "Cherry Picker";
+var startupName = null;
 
 // Put your mock objects here, as in Workshop 4
 var initialData = {
