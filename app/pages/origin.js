254fd8db404be48a120ab35cf71020c282ad05d8
Thomas Aurigemma <taaurigemma@gmail.com>
2017-04-23 14:41:32 -0400

    Forgot to add these files last night. Important part for react-router

---
+++ app/pages/origin.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import LeftNavBar from '../components/leftnavbar.js';
+import Footer from '../components/footer.js';
+import RightSideBar from '../components/rightsidebar.js';
+import {getUserData} from '../server';
+
+export default class Origin extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  refresh() {
+    getUserData("1", (userData) => {
+      this.setState({user: userData});
+    });
+  }
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  render(){
+    return (
+      <div className="container-fluid text-center">
+        <div className="row content">
+          {(this.state !== null) ? <LeftNavBar userData={this.state.user}/> : null }
+          <div className="col-md-7 text-left">
+          {
+            this.props.children
+          }
+          </div>
+          {(this.state !== null) ? <RightSideBar userData={this.state.user}/> : null }
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
