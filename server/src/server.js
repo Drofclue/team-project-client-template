534d5d8349be11ce613eb27c5a9e92864c7dec6d
Thomas Aurigemma <taaurigemma@gmail.com>
2017-04-24 00:53:18 -0400

    The page now starts with the server

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,16 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+// Imports the express Node module.
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+
+// You run the server from `server`, so `../client/build` is `server/../client/build`.
+// '..' means "go up one directory", so this translates into `client/build`!
+app.use(express.static('../client/build'));
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
